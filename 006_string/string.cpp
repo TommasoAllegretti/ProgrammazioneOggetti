1a60605d7bc9372e5e87f99c1a45f2d7b1fdd68c
Tommaso Allegretti <61681908+TommasoAllegretti@users.noreply.github.com>
2020-11-05 10:50:42 +0100

    Create string.cpp

---
+++ 006_string/string.cpp
@@ -0,0 +1,9 @@
+int main(){
+  // stringa in stile C
+  char* st1 = "questa è una stringa";
+  
+  // stringa in stile C++
+  string st2("un'altra stringa");
+  string st3 = "un'altra stringa ancora";
+  
+}
