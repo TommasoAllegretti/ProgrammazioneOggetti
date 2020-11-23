ef58be377050a004f2c3cc3a8e096fb335d73c0e
Tommaso Allegretti <61681908+TommasoAllegretti@users.noreply.github.com>
2020-11-23 03:52:12 +0100

    Create costruttori_mult.cpp

---
+++ 009_costruttori/costruttori_mult.cpp
@@ -0,0 +1,6 @@
+class orario{
+  public:
+    orario();               // costruttore di default
+    orario(int, int);       // costruttore ore-minuti
+    orario(int, int, int);  // costruttore ore-minuti-secondi
+};
