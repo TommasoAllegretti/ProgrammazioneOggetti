d139056d156a214e6f2303b8131121702b5ac4b2
Tommaso Allegretti <61681908+TommasoAllegretti@users.noreply.github.com>
2020-11-23 04:14:47 +0100

    Create altri_modi.cpp

---
+++ 009_costruttori/altri_modi.cpp
@@ -0,0 +1,12 @@
+orario adesso(11, 55);    // costruttore ore-minuti
+
+orario copia;             // costruttore di default
+
+copia = adesso;           // assegnazione
+
+orario copia1 = adesso;   // costruttore di copia
+                          // analogo a: int x = y
+                        
+orario copia2(adesso);    // costruttore di copia
+                          // analogo a: int x(y)
+
