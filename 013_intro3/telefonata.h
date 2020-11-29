215a7ff3bd8868e5deba1e92ee516cf07480d43e
Tommaso Allegretti <61681908+TommasoAllegretti@users.noreply.github.com>
2020-11-29 03:38:59 +0100

    Create telefonata.h

---
+++ 013_intro3/telefonata.h
@@ -0,0 +1,26 @@
+// file "telefonata.h"
+
+#ifndef TELEFONATA_H
+#define TELEFONATA_H
+
+#include <iostream>
+#include "orario.h"
+
+using std::ostream;
+
+class telefonata{
+    public:
+        telefonata(orario, orario, int);
+        telefonata();
+        orario Inizio() const;
+        orario Fine() const;
+        int Numero() const;
+        bool operator==(const telefonata&) const;
+    
+    private:
+        orario inizio, fine;
+        int numero;
+};
+
+ostream& operator<<(ostream&, const telefonata&);
+#endif
